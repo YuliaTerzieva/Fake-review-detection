f1ee2986737710d3f1ef2356d60c7672f03bcf53
YuliaTerzieva <yulia.terzieva@gmail.com>
2023-10-23 17:32:17 +0200

    ready datasets

--- dataset.py
+++ dataset.py
@@ -12,46 +12,47 @@ def separate_dataset(real = True):
     return train, test
 
 
-def make_unigram_dataset():
+def make_n_gram_dataset(n_min, n_max):
     train_true, test_true = separate_dataset()
     flat_train_true = " ".join(train_true)
     flat_test_true = " ".join(test_true)
 
-    count_vec_true = CountVectorizer(stop_words = "english", ngram_range = (1, 1)).fit([flat_train_true])
+    train_fake, test_fake = separate_dataset(real = False)
+    flat_train_fake = " ".join(train_fake)
+    flat_test_fake = " ".join(test_fake)
     
-    X_train_true = count_vec_true.transform(train_true)
+
+    count_vec = CountVectorizer(stop_words = "english", ngram_range = (n_min, n_max)).fit([flat_train_true, flat_train_fake])
+    
+    X_train_true = count_vec.transform(train_true)
     Y_train_true = np.ones(X_train_true.shape[0])
 
-    X_test_true = count_vec_true.transform(test_true)
+    X_test_true = count_vec.transform(test_true)
     Y_test_true = np.ones(X_test_true.shape[0])
 
     ##################
 
-    train_fake, test_fake = separate_dataset(real = False)
-    flat_train_fake = " ".join(train_fake)
-    flat_test_fake = " ".join(test_fake)
-
-    count_vec_fake = CountVectorizer(stop_words = "english", ngram_range = (1, 1)).fit([flat_train_fake])
-    
-    X_train_fake = count_vec_fake.transform(train_fake)
+    X_train_fake = count_vec.transform(train_fake)
     Y_train_fake = np.zeros(X_train_fake.shape[0])
 
-    X_test_fake = count_vec_fake.transform(test_fake)
+    X_test_fake = count_vec.transform(test_fake)
     Y_test_fake = np.zeros(X_test_fake.shape[0])
 
     ############## Combining them 
 
-    X_train = np.vstack(X_train_true, X_train_fake)
-    Y_train = np.vstack(Y_train_true, Y_train_fake)
-    X_test = np.vstack(X_test_true, X_test_fake)
-    Y_test = np.vstack(Y_test_true, Y_test_fake)
+    X_train = np.vstack((X_train_true.toarray(), X_train_fake.toarray()))
+    Y_train = np.vstack((Y_train_true, Y_train_fake))
+    X_test = np.vstack((X_test_true.toarray(), X_test_fake.toarray()))
+    Y_test = np.vstack((Y_test_true, Y_test_fake))
 
-    return X_train, Y_train, X_test, Y_test
+    return X_train, Y_train, X_test, Y_test, count_vec.get_feature_names_out()
 
 
 
 
 if __name__ == "__main__":
-    X_train, Y_train, X_test, Y_test = make_unigram_dataset()
+    X_train, Y_train, X_test, Y_test, feature_names = make_n_gram_dataset(1, 2)
+    print(X_train.shape)
+    print(feature_names)
 
 
--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import string
 
 def read_data(folder_name = "deceptive_from_MTurk"):
     """
-    This one returns a disk of fold - reviews, where reviews are a list of strings 
+    This one returns a dic of fold - reviews, where reviews are a list of strings 
     """
     corpora = {}
     path_to_foulder = "Fake-review-detection/op_spam_v1.4/negative_polarity/" + folder_name
