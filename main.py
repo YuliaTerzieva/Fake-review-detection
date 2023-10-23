0f0f2b3fcc23f979e0fc5aaf03cc3a933607d9ea
YuliaTerzieva <yulia.terzieva@gmail.com>
2023-10-23 15:25:08 +0200

    many new features implemented + new txt files

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from sklearn.decomposition import NMF, PCA
 from sklearn.manifold import TSNE
 import numpy as np
 import os
+import string
 
 
 def read_data(folder_name = "deceptive_from_MTurk"):
@@ -22,25 +23,25 @@ def read_data(folder_name = "deceptive_from_MTurk"):
 
     return corpora
 
-def features_from_laura_link(text):
-    count_vec = CountVectorizer(stop_words = "english")
-    X_count = count_vec.fit_transform(text)
-    print('CountVectorizer:')
-    print(count_vec.get_feature_names_out()[300:310])
-    print(X_count.toarray()[0][300:310]) 
-
-    tfidf_vec = TfidfVectorizer()
-    X_tfidf = tfidf_vec.fit_transform(text)
-    print('TF-IDF:')
-    print(tfidf_vec.get_feature_names_out()[300:310])
-    print(X_tfidf.toarray()[0][300:310])
-
-def most_frequent_words(text, N):
-    count_vec = CountVectorizer(stop_words = "english").fit([text])
+def most_frequent_words(text, N, grams = 1):
+    count_vec = CountVectorizer(stop_words = "english", ngram_range = (grams, grams)).fit([text])
     X_count = count_vec.transform([text])
     words_freq = [(word, X_count[0, idx]) for word, idx in count_vec.vocabulary_.items()]
     words_freq = sorted(words_freq, key = lambda x: x[1], reverse=True)
-    print(words_freq[:N])
+    return words_freq[:N]
+
+def all_words_occuring_once(text, grams = 1):
+    count_vec = CountVectorizer(stop_words = "english", ngram_range = (grams, grams)).fit([text])
+    X_count = count_vec.transform([text])
+    words_freq = [(word, X_count[0, idx]) for word, idx in count_vec.vocabulary_.items()]
+    single_words = [word for (word, count) in words_freq if count == 1]
+    return single_words
+    
+
+def average_count_punctuation(corpora):
+    punctuation_count = sum(1 for review in corpora for char in review if char in string.punctuation)
+    return punctuation_count/len(corpora)
+
                        
 
 if __name__ == "__main__":
@@ -65,26 +66,40 @@ if __name__ == "__main__":
     lens_fake_corpora = [len(review.split(" ")) for review in all_text_from_fake_corpora]
     avg_len_fake_corpora = sum(lens_fake_corpora)/len(lens_fake_corpora)
 
-    print("The average length of a true review is : ", avg_len_true_corpora, ", where as a fake one has an average of ", avg_len_fake_corpora)
+    print("\n ################################################################## \n")
+    print("The average length of a true review is : ", avg_len_true_corpora, "\n\
+           where as a fake one has an average of ", avg_len_fake_corpora)
     
-    # features_from_laura_link(all_text_from_true_corpora)
-    ## The results are not that good. we need to do sth else, this is houwing the documtne-term matrix i.e. in each review how many time does each word occur. This is not useful. 
-
+    print("\n ################################################################## \n")
+    print("The average punctuation in a true review is : ", average_count_punctuation(all_text_from_true_corpora), "\n\
+            where for a fake one is ", average_count_punctuation(all_text_from_fake_corpora))
+    
     ### finding the most common N words : 
+    print("\n ################################################################## \n")
     print("The 20 most frequent words from the true corpora are: ")
-    most_frequent_words(all_text_from_true_corpora_flatten, 20)
+    print(most_frequent_words(all_text_from_true_corpora_flatten, 20, 1))
 
     print("The 20 most frequent words from the fake corpora are: ")
-    most_frequent_words(all_text_from_fake_corpora_flatten, 20)
+    print(most_frequent_words(all_text_from_fake_corpora_flatten, 20, 1))
 
-    '''
-    Suggested features 
-     - len of review
-     - how many dots, columns, sth are used, normalized by the length
-     - 1) we can find the most frequent words in the corpora and then 2) see how many words in the review are in the 10/20/30 most frequent words used (also normalized by length?)
-    '''
+    print("\n ################################################################## \n")
+    print("The 20 most frequent bigrams from the true corpora are: ")
+    print(most_frequent_words(all_text_from_true_corpora_flatten, 20, 2))
+    
+    print("The 20 most frequent bigrams from the fake corpora are: ")
+    print(most_frequent_words(all_text_from_fake_corpora_flatten, 20, 2))
 
+    print("\n ################################################################## \n")
+    print("The 20 most frequent trigrams from the true corpora are: ")
+    print(most_frequent_words(all_text_from_true_corpora_flatten, 20, 3))
 
+    print("The 20 most frequent trigrams from the fake corpora are: ")
+    print(most_frequent_words(all_text_from_fake_corpora_flatten, 20, 3))
 
+    print("\n ################################################################## \n")
+    print("The words that appear just once from the true corpora are: ")
+    print(all_words_occuring_once(all_text_from_true_corpora_flatten, 1))
 
+    print("The words that appear just once from the fake corpora are: ")
+    print(all_words_occuring_once(all_text_from_fake_corpora_flatten, 1))
 
