adecd1134018ad8dc3062bd4d129003f2007aec9
Raoul Brigola <r.brigola@gmx.de>
2023-10-26 14:16:24 +0200

    t tests for all classifier pairs; only best found parameters in param-grids for faster execution

--- classifiers.py
+++ classifiers.py
@@ -1,5 +1,4 @@
 import time
-from itertools import combinations
 
 import numpy as np
 from sklearn.naive_bayes import MultinomialNB
@@ -9,12 +8,13 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, confusion_matrix
 from sklearn import metrics
-from mlxtend.evaluate import paired_ttest_5x2cv
+from scipy import stats
+from itertools import combinations
 
 from Data import *
 
 
-def calculate_metrics(label, pred, clf):
+def print_metrics(label, pred, clf):
     precision = metrics.precision_score(label, pred, average=None)
     recall = metrics.recall_score(label, pred, average=None)
     f1 = metrics.f1_score(label, pred, average=None)
@@ -37,9 +37,13 @@ def grid_search(model, param_grid, X_train, y_train, X_test, y_test):
     print('\033[94m' + "Accuracy with below parameters: ", accuracy)
     print(clf.best_params_)
 
-    calculate_metrics(y_test, pred, clf)
+    print_metrics(y_test, pred, clf)
 
-    return clf
+    print("Class name: ", model.__class__.__name__)
+    return model.__class__.__name__, pred
+
+
+#######################################################################################################################
 
 
 # measuring runtime
@@ -52,21 +56,25 @@ data_bigrams = Data(2)
 X_train_uni, y_train_uni, X_test_uni, y_test_uni, feature_names_uni = data_unigrams.X_train, data_unigrams.Y_train, data_unigrams.X_test, data_unigrams.Y_test, data_unigrams.feature_names
 X_train_bi, y_train_bi, X_test_bi, y_test_bi, feature_names_bi = data_bigrams.X_train, data_bigrams.Y_train, data_bigrams.X_test, data_bigrams.Y_test, data_bigrams.feature_names
 
-all_classifiers = []
+
+#######################################################################################################################
+
+
+all_classifiers = {}
 
 # 1. Classifier: Multinomial NB
 model = MultinomialNB(force_alpha=True)
 param_grid = {
-    "alpha": list(np.linspace(0.1, 2, num=11)),
+    "alpha": [0.48],  # list(np.linspace(0.1, 2, num=11)),
     "fit_prior": [True, False]
 }
 print("\n")
 print('\033[92m' + "Multinomial Naive Bayes with unigram features:")
-clf_multinb_uni = grid_search(model, param_grid, X_train_uni, y_train_uni, X_test_uni, y_test_uni)
-all_classifiers.append(clf_multinb_uni)
+clf_multinb_uni, pred_multinb_uni = grid_search(model, param_grid, X_train_uni, y_train_uni, X_test_uni, y_test_uni)
+all_classifiers[clf_multinb_uni + "_uni"] = pred_multinb_uni
 print('\033[92m' + "Multinomial Naive Bayes with unigram and bigram features:")
-clf_multinb_bi = grid_search(model, param_grid, X_train_bi, y_train_bi, X_test_bi, y_test_bi)
-all_classifiers.append(clf_multinb_bi)
+clf_multinb_bi, pred_multinb_bi = grid_search(model, param_grid, X_train_bi, y_train_bi, X_test_bi, y_test_bi)
+all_classifiers[clf_multinb_bi + "_bi"] = pred_multinb_bi
 
 
 # 2. Classifier: Logistic regression
@@ -74,62 +82,73 @@ model = LogisticRegression()
 param_grid = {
     "penalty": ["l1"],  # lasso penalty
     "solver": ["liblinear"],
-    "C": list(np.ones(10) - (np.logspace(0, 2, num=10) / 100) + 0.01),
+    "C": [1],  # list(np.ones(10) - (np.logspace(0, 2, num=10) / 100) + 0.01),
 }
 print("\n")
 print('\033[92m' + "Logistic Regression with unigram features:")
-clf_logreg_uni = grid_search(model, param_grid, X_train_uni, y_train_uni, X_test_uni, y_test_uni)
-all_classifiers.append(clf_logreg_uni)
+clf_logreg_uni, pred_logreg_uni = grid_search(model, param_grid, X_train_uni, y_train_uni, X_test_uni, y_test_uni)
+all_classifiers[clf_logreg_uni + "_uni"] = pred_logreg_uni
 print('\033[92m' + "Logistic Regression with unigram and bigram features:")
-clf_logreg_bi = grid_search(model, param_grid, X_train_bi, y_train_bi, X_test_bi, y_test_bi)
-all_classifiers.append(clf_logreg_bi)
+clf_logreg_bi, pred_logreg_bi = grid_search(model, param_grid, X_train_bi, y_train_bi, X_test_bi, y_test_bi)
+all_classifiers[clf_logreg_bi + "_bi"] = pred_logreg_bi
 
 
 # 3. Classifier: Classification trees
 model = DecisionTreeClassifier()
 param_grid = {
-    "criterion": ["gini", "entropy", "log_loss"],  # ToDo: maybe delete if takes too long
-    "max_depth": [1],  # ToDo: choose parameters
-    "min_samples_split": [2],
-    "min_samples_leaf": [1],
+    "criterion": ["gini", "entropy", "log_loss"],
+    "max_depth": [2],  # list(np.arange(5) + 1),
+    "min_samples_split": [2],  # list(np.arange(5) + 1),
+    "min_samples_leaf": [1],  # list(np.arange(5) + 1),
     "ccp_alpha": [0.0],
 }
 print("\n")
 print('\033[92m' + "Classification trees with unigram features:")
-clf_ctrees_uni = grid_search(model, param_grid, X_train_uni, y_train_uni, X_test_uni, y_test_uni)
-all_classifiers.append(clf_ctrees_uni)
+clf_ctrees_uni, pred_ctrees_uni = grid_search(model, param_grid, X_train_uni, y_train_uni, X_test_uni, y_test_uni)
+all_classifiers[clf_ctrees_uni + "_uni"] = pred_ctrees_uni
 print('\033[92m' + "Classification trees with unigram and bigram features:")
-clf_ctrees_bi = grid_search(model, param_grid, X_train_bi, y_train_bi, X_test_bi, y_test_bi)
-all_classifiers.append(clf_ctrees_bi)
+clf_ctrees_bi, pred_ctrees_bi = grid_search(model, param_grid, X_train_bi, y_train_bi, X_test_bi, y_test_bi)
+all_classifiers[clf_ctrees_bi + "_bi"] = pred_ctrees_bi
 
 
 # 4. Classifier: Random forests
 model = RandomForestClassifier()
 param_grid = {
-    "n_estimators": [100],  # ToDo: choose parameters
+    "n_estimators": [500],  # [10, 20, 50, 100, 200, 500],
     "criterion": ["gini", "entropy", "log_loss"],
-    "max_depth": [1],
-    "min_samples_split": [2],
-    "min_samples_leaf": [1],
-    "ccp_alpha": [0.0],
+    "max_depth": [2],  # list(np.arange(5) + 1),
+    "min_samples_split": [2],  # list(np.arange(5) + 1),
+    "min_samples_leaf": [1],  # list(np.arange(5) + 1),
+    "ccp_alpha": [0.0],  # ToDo: choose parameters
 }
 print("\n")
 print('\033[92m' + "Random Forests with unigram features:")
-clf_randforest_uni = grid_search(model, param_grid, X_train_uni, y_train_uni, X_test_uni, y_test_uni)
-all_classifiers.append(clf_randforest_uni)
+clf_randforest_uni, pred_randforest_uni = grid_search(model, param_grid, X_train_uni, y_train_uni, X_test_uni, y_test_uni)
+all_classifiers[clf_randforest_uni + "_uni"] = pred_randforest_uni
 print('\033[92m' + "Random forest with unigram and bigram features:")
-clf_ranforest_bi = grid_search(model, param_grid, X_train_bi, y_train_bi, X_test_bi, y_test_bi)
-all_classifiers.append(clf_ranforest_bi)
+clf_randforest_bi, pred_randforest_bi = grid_search(model, param_grid, X_train_bi, y_train_bi, X_test_bi, y_test_bi)
+all_classifiers[clf_randforest_bi + "_bi"] = pred_randforest_bi
 
 
-def accuracy_t_test(pair):  # ToDo: implement t test for two classifiers + print relevant results
-    pass
+#######################################################################################################################
 
 
-# ToDo: put classifiers in dict with name as key and predictions as value
-# pairs_of_models = list(map(dict, combinations(all_classifiers.items(), 2)))
-# for pair in pairs_of_models:
-  #  accuracy_t_test(pair)
+def accuracy_t_test(model_pair, model_dict):
+    pred1 = model_dict[model_pair[0]]
+    pred2 = model_dict[model_pair[1]]
+    t_statistic, p_value = stats.ttest_rel(pred1, pred2)
+
+    print("Paired t-test results for classifiers ", model_pair[0], " and ", model_pair[1], ": ")
+    if p_value < 0.05:
+        print("Difference is significant!")
+    print("T-statistic:", t_statistic)
+    print("P-value:", p_value)
+
+
+# calculate pairwise t-tests
+model_pairs = list(combinations(list(all_classifiers.keys()), 2))
+for pair in model_pairs:
+    accuracy_t_test(pair, all_classifiers)
 
 
 # measuring runtime
